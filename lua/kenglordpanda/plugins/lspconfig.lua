a9d914719a75cfe93ef893961359e83c0ffe3ab9
Khang Tran <ktran7246@gmail.com>
2024-03-05 13:21:31 -0600

    got completion to work

--- lua/kenglordpanda/plugins/lspconfig.lua
+++ lua/kenglordpanda/plugins/lspconfig.lua
@@ -1,52 +1,48 @@
 return {
 
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-	},
 
-	{
-		"williamboman/mason.nvim",
-	},
+    {
+        "williamboman/mason.nvim",
+    },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-	},
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-		config = function(_, opts)
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					javascript = { "prettierd" },
-					typescript = { "prettierd " },
-					vue = { "prettierd" },
-					html = { "prettierd" },
-					css = { "prettierd" },
-				},
-				format_on_save = {
-					timeout_ms = 500,
-					lsp_fallback = true,
-				},
-			})
-		end,
-	},
-	{
-		"mfussenegger/nvim-lint",
-		config = function()
-			local lint = require("lint")
-			lint.linters_by_ft = {
-				lua = { "luacheck" },
-				javascript = { "eslint_d" },
-				typescript = { "eslint_d" },
-				css = { "stylelint" },
-			}
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-				callback = function()
-					require("lint").try_lint()
-				end,
-			})
-		end,
-	},
+    {
+        "williamboman/mason-lspconfig.nvim",
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+        config = function(_, opts)
+            require("conform").setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    javascript = { "prettierd" },
+                    typescript = { "prettierd " },
+                    vue = { "prettierd" },
+                    html = { "prettierd" },
+                    css = { "prettierd" },
+                },
+                format_on_save = {
+                    timeout_ms = 500,
+                    lsp_fallback = true,
+                },
+            })
+        end,
+    },
+    {
+        "mfussenegger/nvim-lint",
+        config = function()
+            local lint = require("lint")
+            lint.linters_by_ft = {
+                lua = { "luacheck" },
+                javascript = { "eslint_d" },
+                typescript = { "eslint_d" },
+                css = { "stylelint" },
+            }
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = function()
+                    require("lint").try_lint()
+                end,
+            })
+        end,
+    },
 }
